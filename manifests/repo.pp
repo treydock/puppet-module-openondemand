ccd63694434038858bdbc7aa571c3286d2084809
Trey Dockendorf <tdockendorf@osc.edu>
2018-02-27 09:55:25 -0500

    Set metadata_expire=1 for ondemand yum repo to allow updates to be readily available

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -7,11 +7,12 @@ class openondemand::repo {
   $baseurl = "${openondemand::repo_baseurl_prefix}/${openondemand::repo_release}/web/el${facts['os']['release']['major']}/\$basearch"
 
   yumrepo { 'ondemand-web':
-    descr    => 'Open OnDemand Web Repo',
-    baseurl  => $baseurl,
-    enabled  => '1',
-    gpgcheck => '1',
-    gpgkey   => $openondemand::repo_gpgkey,
+    descr           => 'Open OnDemand Web Repo',
+    baseurl         => $baseurl,
+    enabled         => '1',
+    gpgcheck        => '1',
+    gpgkey          => $openondemand::repo_gpgkey,
+    metadata_expire => '1'
   }
 
 }

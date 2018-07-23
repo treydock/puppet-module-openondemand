bf42cb257190cfc605b9a5409ab2ddda7ec5ea08
Trey Dockendorf <tdockendorf@osc.edu>
2018-07-23 17:19:34 -0400

    Support removing usr shared apps from ondemand

--- manifests/app/usr.pp
+++ manifests/app/usr.pp
@@ -1,5 +1,6 @@
 #
 define openondemand::app::usr (
+  Enum['present','absent'] $ensure = 'present',
   $mode = '0750',
   $owner = 'root',
   $group = 'root',
@@ -13,18 +14,33 @@ define openondemand::app::usr (
   $gateway      = "${web_dir}/gateway"
   $gateway_src  = "~${name}/${home_subdir}"
 
-  file { $web_dir:
-    ensure => 'directory',
-    owner  => $owner,
-    group  => $group,
-    mode   => $mode,
+  if $ensure == 'present' {
+    file { $web_dir:
+      ensure => 'directory',
+      owner  => $owner,
+      group  => $group,
+      mode   => $mode,
+    }
+
+    exec { "openondemand::app::usr-link-gateway-${name}":
+      path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+      command => "ln -s ${gateway_src} ${gateway}",
+      unless  => "test -L ${gateway}",
+      require => File[$web_dir]
+    }
   }
 
-  exec { "openondemand::app::usr-link-gateway-${name}":
-    path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-    command => "ln -s ${gateway_src} ${gateway}",
-    unless  => "test -L ${gateway}",
-    require => File[$web_dir]
+  if $ensure == 'absent' {
+    exec { "openondemand::app::usr-unlink-gateway-${name}":
+      path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+      command => "unlink ${gateway}",
+      onlyif  => "test -L ${gateway}",
+      before => File[$web_dir]
+    }
+    file { $web_dir:
+      ensure => 'absent',
+    }
   }
+
 
 }

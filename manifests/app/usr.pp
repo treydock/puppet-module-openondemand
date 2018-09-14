d84cac09bb901a8c1f9969470d6c1e175502ed21
Trey Dockendorf <tdockendorf@osc.edu>
2018-09-14 15:51:35 -0400

    Can't reassign variable

--- manifests/app/usr.pp
+++ manifests/app/usr.pp
@@ -13,7 +13,7 @@ define openondemand::app::usr (
   $base_web_dir = "${openondemand::_web_directory}/apps/usr"
   $web_dir      = "${base_web_dir}/${name}"
   $gateway      = "${web_dir}/gateway"
-  $gateway_src  = pick($gateway_src, "~${name}/${home_subdir}")
+  $_gateway_src  = pick($gateway_src, "~${name}/${home_subdir}")
 
   if $ensure == 'present' {
     file { $web_dir:
@@ -25,7 +25,7 @@ define openondemand::app::usr (
 
     exec { "openondemand::app::usr-link-gateway-${name}":
       path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-      command => "ln -s ${gateway_src} ${gateway}",
+      command => "ln -s ${_gateway_src} ${gateway}",
       unless  => "test -L ${gateway}",
       require => File[$web_dir]
     }

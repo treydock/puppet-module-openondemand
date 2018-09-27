29833b102548165838f2d14cd56a0ecd549e1509
Eric Franz <efranz@osc.edu>
2018-09-27 13:07:28 -0400

    specify gateway_src for all ondemand::app::usr
    explicitly specify gateway_src for every user, instead of assuming
    it is a home directory and trying to resolve the directory location
    
    this ensures that puppet can properly enforce modifications to gateway_src

--- manifests/app/usr.pp
+++ manifests/app/usr.pp
@@ -1,11 +1,10 @@
 #
 define openondemand::app::usr (
+  $gateway_src,
   Enum['present','absent'] $ensure = 'present',
   $mode = '0750',
   $owner = 'root',
   $group = 'root',
-  $home_subdir = 'ood/share',
-  $gateway_src = undef,
 ) {
 
   include openondemand
@@ -13,7 +12,6 @@ define openondemand::app::usr (
   $base_web_dir = "${openondemand::_web_directory}/apps/usr"
   $web_dir      = "${base_web_dir}/${name}"
   $gateway      = "${web_dir}/gateway"
-  $_gateway_src  = pick($gateway_src, "~${name}/${home_subdir}")
 
   if $ensure == 'present' {
     file { $web_dir:
@@ -25,7 +23,7 @@ define openondemand::app::usr (
 
     exec { "openondemand::app::usr-link-gateway-${name}":
       path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-      command => "ln -s ${_gateway_src} ${gateway}",
+      command => "ln -s ${gateway_src} ${gateway}",
       unless  => "test -L ${gateway}",
       require => File[$web_dir]
     }
